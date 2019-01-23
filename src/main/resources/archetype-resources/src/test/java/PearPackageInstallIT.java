b818888f28562e5960a43e1c0c9d071dc5bd06f8
Christian Gaege <christian.gaege@averbis.com>
2019-01-23 21:27:22 +0100

    Added testProcess() integration test

--- src/main/resources/archetype-resources/src/test/java/PearPackageInstallIT.java
+++ src/main/resources/archetype-resources/src/test/java/PearPackageInstallIT.java
@@ -1,7 +1,13 @@
 package ${package};
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.pear.tools.PackageInstaller;
 import org.junit.Test;
 
@@ -14,6 +20,16 @@ public class PearPackageInstallIT {
 		File pearPackage = new File("target/${artifactId}-${version}.pear");
 		
 		PackageInstaller.installPackage(installDir, pearPackage, true);
+	}
+	
+	@Test
+	public void testProcess() throws IOException, UIMAException {
+		
+		AnalysisEngine analysisEngine = AnalysisEngineFactory.createEngine("${packageInPathFormat}/${artifactId}RutaAnnotator");
+		JCas jCas = JCasFactory.createJCas();
+		jCas.setDocumentText("Test document text");
+		
+		analysisEngine.process(jCas);
 	}
 
 }
--- src/test/resources/projects/it2/reference/src/test/java/de/customer/nlp/PearPackageInstallIT.java
+++ src/test/resources/projects/it2/reference/src/test/java/de/customer/nlp/PearPackageInstallIT.java
@@ -1,7 +1,13 @@
 package de.customer.nlp;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.pear.tools.PackageInstaller;
 import org.junit.Test;
 
@@ -14,6 +20,16 @@ public class PearPackageInstallIT {
 		File pearPackage = new File("target/MyAnnotator-1.pear");
 		
 		PackageInstaller.installPackage(installDir, pearPackage, true);
+	}
+	
+	@Test
+	public void testProcess() throws IOException, UIMAException {
+		
+		AnalysisEngine analysisEngine = AnalysisEngineFactory.createEngine("de/customer/nlp/MyAnnotatorRutaAnnotator");
+		JCas jCas = JCasFactory.createJCas();
+		jCas.setDocumentText("Test document text");
+		
+		analysisEngine.process(jCas);
 	}
 
 }
